d2f898c0d195f44083f70d5d18a73baca08e4b2e
Theo Pavlakou <theo.pavlakou@gmail.com>
2013-05-22 17:12:17 +0100

    Changes in get_motionless.m

--- Matlab/get_motionless.m
+++ Matlab/get_motionless.m
@@ -5,37 +5,37 @@ function frame = get_motionless(data_mat,win_len)
     values = zeros(1,24);
 
     % left elbow
-    values(1) = get_motionless_frame(data_mat(:,17),win_len);
-    values(2) = get_motionless_frame(data_mat(:,18),win_len);
-    values(3) = get_motionless_frame(data_mat(:,19),win_len);
+    values(1) = get_motionless_frame(data_mat(:,index_of_joint('left_elbow')+0),win_len);
+    values(2) = get_motionless_frame(data_mat(:,index_of_joint('left_elbow')+1),win_len);
+    values(3) = get_motionless_frame(data_mat(:,index_of_joint('left_elbow')+2),win_len);
     % right elbow
-    values(4) = get_motionless_frame(data_mat(:,21),win_len);
-    values(5) = get_motionless_frame(data_mat(:,22),win_len);
-    values(6) = get_motionless_frame(data_mat(:,23),win_len);
+    values(4) = get_motionless_frame(data_mat(:,index_of_joint('right_elbow')+0),win_len);
+    values(5) = get_motionless_frame(data_mat(:,index_of_joint('right_elbow')+1),win_len);
+    values(6) = get_motionless_frame(data_mat(:,index_of_joint('right_elbow')+2),win_len);
     % left hand
-    values(7) = get_motionless_frame(data_mat(:,25),win_len);
-    values(8) = get_motionless_frame(data_mat(:,26),win_len);
-    values(9) = get_motionless_frame(data_mat(:,27),win_len);
+    values(7) = get_motionless_frame(data_mat(:,index_of_joint('left_hand')+0),win_len);
+    values(8) = get_motionless_frame(data_mat(:,index_of_joint('left_hand')+1),win_len);
+    values(9) = get_motionless_frame(data_mat(:,index_of_joint('left_hand')+2),win_len);
     % right hand
-    values(10) = get_motionless_frame(data_mat(:,29),win_len);
-    values(11) = get_motionless_frame(data_mat(:,30),win_len);
-    values(12) = get_motionless_frame(data_mat(:,31),win_len);
+    values(10) = get_motionless_frame(data_mat(:,index_of_joint('right_hand')+0),win_len);
+    values(11) = get_motionless_frame(data_mat(:,index_of_joint('right_hand')+1),win_len);
+    values(12) = get_motionless_frame(data_mat(:,index_of_joint('right_hand')+2),win_len);
     % left knee
-    values(13) = get_motionless_frame(data_mat(:,45),win_len);
-    values(14) = get_motionless_frame(data_mat(:,46),win_len);
-    values(15) = get_motionless_frame(data_mat(:,47),win_len);
+    values(13) = get_motionless_frame(data_mat(:,index_of_joint('left_knee')+0),win_len);
+    values(14) = get_motionless_frame(data_mat(:,index_of_joint('left_knee')+1),win_len);
+    values(15) = get_motionless_frame(data_mat(:,index_of_joint('left_knee')+2),win_len);
     % left foot
-    values(16) = get_motionless_frame(data_mat(:,49),win_len);
-    values(17) = get_motionless_frame(data_mat(:,50),win_len);
-    values(18) = get_motionless_frame(data_mat(:,51),win_len);
+    values(16) = get_motionless_frame(data_mat(:,index_of_joint('left_foot')+0),win_len);
+    values(17) = get_motionless_frame(data_mat(:,index_of_joint('left_foot')+1),win_len);
+    values(18) = get_motionless_frame(data_mat(:,index_of_joint('left_foot')+2),win_len);
     % right knee
-    values(19) = get_motionless_frame(data_mat(:,53),win_len);
-    values(20) = get_motionless_frame(data_mat(:,54),win_len);
-    values(21) = get_motionless_frame(data_mat(:,55),win_len);
+    values(19) = get_motionless_frame(data_mat(:,index_of_joint('right_knee')+0),win_len);
+    values(20) = get_motionless_frame(data_mat(:,index_of_joint('right_knee')+1),win_len);
+    values(21) = get_motionless_frame(data_mat(:,index_of_joint('right_knee')+2),win_len);
     % right foot
-    values(22) = get_motionless_frame(data_mat(:,57),win_len);
-    values(23) = get_motionless_frame(data_mat(:,58),win_len);
-    values(24) = get_motionless_frame(data_mat(:,59),win_len);
+    values(22) = get_motionless_frame(data_mat(:,index_of_joint('right_foot')+0),win_len);
+    values(23) = get_motionless_frame(data_mat(:,index_of_joint('right_foot')+1),win_len);
+    values(24) = get_motionless_frame(data_mat(:,index_of_joint('right_foot')+2),win_len);
 
     % get unique frame numbers in values array
     unique_el = unique(values);
