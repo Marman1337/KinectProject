4a3f9ec7d48759470afb8796cc4b80da5c5d25f8
Marcin <marman1@gmail.com>
2013-05-23 11:56:43 +0100

    comments in delay_estimate.m

--- Matlab/delay_estimate.m
+++ Matlab/delay_estimate.m
@@ -28,12 +28,16 @@ function [ delay_est ] = delay_estimate( data_joints_1, data_joints_2 )
     % remove the cross-correlation offset
     x_delay = x_delay - offset_pt;
     y_delay = y_delay - offset_pt;
-    
+
+    % Calculate the mean and std of the delay offsets, and use them later
+    % to estimate the delay only using the values which lie within one
+    % standard deviation from the mean, i.e. eliminate outliers
     x_mean = mean(x_delay);
     y_mean = mean(y_delay);
     x_std = std(x_delay);
     y_std = std(y_delay);
 
+    % Identify outliers, accumulate the delays which are within one std
     sum_del = 0;
     count_el = 0;
     for i = 1:15
@@ -47,9 +51,7 @@ function [ delay_est ] = delay_estimate( data_joints_1, data_joints_2 )
         end
     end
     
+    % Estimate the frame shift by averaging
     delay_est = round(sum_del / count_el);
-    % calculate overall average delay
-    %delay_est = round((sum(x_delay)/numel(x_delay) + sum(y_delay)/numel(y_delay))/2);
 
 end
-
