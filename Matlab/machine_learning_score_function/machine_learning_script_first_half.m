0f838a52f5c4757be9dacb7bab3473e09b322010
Theo Pavlakou <theo.pavlakou@gmail.com>
2013-06-05 16:24:55 +0100

    Adds scripts that tests machine learning algorithms on 'Score Data'

--- Matlab/machine_learning_score_function/machine_learning_script_first_half.m
+++ Matlab/machine_learning_score_function/machine_learning_script_first_half.m
@@ -2,9 +2,12 @@
 close all;
 % x =  0:0.1:10;
 % y = build_polynomial([1 -2 +3]',x);
-x = 0:0.125:1.25;
+% x = 0:0.125:1.25;
+x = 0:0.125:0.75;
 
-y = [100; 98.5; 95; 85; 70; 50; 30; 12.5; 5; 3; 2.2];
+y = [100; 98.5; 95; 85; 70; 50; 30];
+% y = [100; 98.5; 95; 85; 70; 50; 30; 12.5; 5; 3; 2.2];
+
 %% Add noise to data to resemble more realistic data.
 %y = y + 10*randn(size(y));
 data_set = [x' y];
@@ -17,10 +20,10 @@ plot(x, y, 'x');
 %% Find theta for different orders of hypothesis.
 max_order = 7;
 % Some gradient descent settings
-iterations = 450000;
-alpha = 0.0001;
+iterations = 1000000;
+alpha = 0.001;
     
-[ theta_order_mat ] = theta_order_matrix( training_set, max_order, iterations, alpha );
+[ theta_order_mat ] = theta_order_matrix_specialised( training_set, max_order, iterations, alpha );
 
 %% Find order of hypothesis that minimizes the cross validation cost function.
 [ ~, best_order ] = order_theta_least_cost( cv_set, theta_order_mat );
@@ -29,7 +32,7 @@ alpha = 0.0001;
 
 %% Iterate over different values of lambda to get the different values of theta.
 lambda_range = [0.0001 0.001 0.01 0.1 0.2 0.4 0.8 1.6 3.2 6.4 12.8];
-[ theta_lambda_mat ] = theta_lambda_matrix( training_set, lambda_range, iterations, alpha, best_order  );
+[ theta_lambda_mat ] = theta_lambda_matrix_specialised( training_set, lambda_range, iterations, alpha, best_order  );
 
 %% Find theta which gives lowest cost. This will have the same index as the lambda
 %  that gives the lowest cost. Use the cross validation cost function.
--- Matlab/machine_learning_score_function/machine_learning_script_second_half.m
+++ Matlab/machine_learning_score_function/machine_learning_script_second_half.m
@@ -1,10 +1,12 @@
 %% Input data: mx2 matrix of (x, y) tuples.
-close all;
 % x =  0:0.1:10;
 % y = build_polynomial([1 -2 +3]',x);
-x = 0:0.125:1.25;
+% x = 0:0.125:1.25;
+x = 0.75:0.125:1.5;
 
-y = [100; 98.5; 95; 85; 70; 50; 30; 12.5; 5; 3; 2.2];
+% y = [100; 98.5; 95; 85; 70; 50; 30];
+y = [12.5; 5; 3; 2.2; 1.9; 1.7; 1.6];
+
 %% Add noise to data to resemble more realistic data.
 %y = y + 10*randn(size(y));
 data_set = [x' y];
@@ -15,12 +17,12 @@ plot(x, y, 'x');
 [training_set, cv_set, test_set] = split_data_set(data_set);
 
 %% Find theta for different orders of hypothesis.
-max_order = 7;
+max_order = 5;
 % Some gradient descent settings
-iterations = 450000;
-alpha = 0.0001;
+iterations = 1000000;
+alpha = 0.001;
     
-[ theta_order_mat ] = theta_order_matrix( training_set, max_order, iterations, alpha );
+[ theta_order_mat ] = theta_order_matrix_specialised( training_set, max_order, iterations, alpha );
 
 %% Find order of hypothesis that minimizes the cross validation cost function.
 [ ~, best_order ] = order_theta_least_cost( cv_set, theta_order_mat );
@@ -29,7 +31,7 @@ alpha = 0.0001;
 
 %% Iterate over different values of lambda to get the different values of theta.
 lambda_range = [0.0001 0.001 0.01 0.1 0.2 0.4 0.8 1.6 3.2 6.4 12.8];
-[ theta_lambda_mat ] = theta_lambda_matrix( training_set, lambda_range, iterations, alpha, best_order  );
+[ theta_lambda_mat ] = theta_lambda_matrix_specialised( training_set, lambda_range, iterations, alpha, best_order  );
 
 %% Find theta which gives lowest cost. This will have the same index as the lambda
 %  that gives the lowest cost. Use the cross validation cost function.
@@ -42,7 +44,7 @@ best_lambda = lambda_range(best_lambda_index);
 %% Plot data
 y_new = build_polynomial(theta ,x);
 hold on;
-plot(x, y_new, 'r');
+plot(x, y_new, 'g');
 title('Score vs Error');
 legend('Data Set', 'Estimate');
 xlabel('Error');
